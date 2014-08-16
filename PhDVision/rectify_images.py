d00ef29162287a52b0ef86f0109348cbf8de9cbf
Andrew Schechtman-Rook <rook166@gmail.com>
2014-08-16 11:35:39 -0400

    Standalone image rectifier done

--- PhDVision/rectify_images.py
+++ PhDVision/rectify_images.py
@@ -9,18 +9,18 @@ import os
 import sys
 import bottleneck as bn
 
-from alignlib import compute_frameorder
+from alignlib import compute_frameorder,frame,match_frames_twosided,compute_extrema,adj_mask_or,insert_image
 
 if __name__ == "__main__":
     if len(sys.argv) != 7:
         sys.exit('Syntax: [Video Filename] [First Frame #] [Last Frame #] [Frames to Skip (1 for no skip)] [Output Images Prefix] [Print Original Images (Y/N)]')
     
     #Setup video and frame range
-    filename = sys.argv[1]#'/Users/Rook/nflstats/nflvids/nyj/2013/2013100700.mp4'
+    filename = sys.argv[1]
     cap = ski_io.Video(filename)
-    firstframe = int(sys.argv[2])#8750#2690
-    lastframe = int(sys.argv[3])8890#2750
-    startframe = firstframe + (lastframe-firstframe)//2#8820#2720
+    firstframe = int(sys.argv[2])
+    lastframe = int(sys.argv[3])
+    startframe = firstframe + (lastframe-firstframe)//2
     frameskip = int(sys.argv[4])
     outprefix = sys.argv[5]
     printorig = False
@@ -42,23 +42,24 @@ if __name__ == "__main__":
     imagearr = np.zeros((currframe.rawimage.shape[0],currframe.rawimage.shape[1],currframe.rawimage.shape[2],len(frameorder)))/0.
     imagearr[:,:,:,0] = ski_exp.rescale_intensity(ski_util.img_as_float(currframe.rawimage))
     #Go through the frames:
+    print "Aligning Frames:"
     for i in range(1,len(frameorder)):
-        print i,len(frameorder)-1,frameorder[i],imagearr.shape
+        print "Currently aligning frame {0:d} of {1:d} (frame number {2:d}), master image array shape =".format(i,len(frameorder)-1,frameorder[i]),imagearr.shape
         newframe = frame(cap,frameorder[i])
+        #Output the original frames, if necessary:
+        if printorig:
+            ski_io.imsave(outprefix+'_orig_{0:d}.jpg'.format(frameorder[i]),newframe.rawimage)
         #Compute matches:
         matches = match_frames_twosided(currframe,newframe,dist_ratio=0.6)
         matching_coords_curr = currframe.coords[matches > 0,:]
         matching_coords_new = newframe.coords[matches[matches>0],:]
         #Compute homology:
         model,inliers = ski_measure.ransac((matching_coords_curr,matching_coords_new),ski_trans.ProjectiveTransform,min_samples=25,residual_threshold=1.0,max_trials=2000)
-        test = homology_chisquared(newframe.rawimage,ski_exp.rescale_intensity(ski_util.img_as_float(currframe.rawimage)),model._matrix.reshape(-1))
         homologies[i,:,:] = model._matrix
         #Determine where the new frame extrema would be after being transformed:
         xmin,xmax,ymin,ymax = compute_extrema(newframe,model._matrix)
         jointimage = ski_exp.rescale_intensity(ski_util.img_as_float(currframe.rawimage.copy()))
-        #ax = plot_matches(newframe.rawimage,currframe.rawimage,matching_coords_new[inliers,:],matching_coords_new[inliers,:],matches[inliers],show_below=False,ax=None)
-        #ax.figure.savefig('test_matches_{0:d}.jpg'.format(newframe.framenumber))
-        #print xmin,ymin,xmax,ymax,imagearr.shape,np.sum(inliers)
+        #Make sure things haven't gone totally awry:
         if xmax > currframe.rawimage.shape[1]*1.5 or ymax > currframe.rawimage.shape[0]*1.5 or xmin < currframe.rawimage.shape[1]*-0.5 or ymin < currframe.rawimage.shape[0]*-0.5:
             ski_io.imsave('test_avg_aborted.jpg',currframe.rawimage)
             print "    ",xmin,xmax,ymin,ymax,currframe.rawimage.shape
@@ -92,10 +93,8 @@ if __name__ == "__main__":
         transform = ski_trans.ProjectiveTransform(matrix=homologies[i,:,:])
         unwarped_newimage = ski_trans.warp(newframe.rawimage,transform,output_shape=jointimage.shape,cval=-10)
         newweight = (unwarped_newimage[:,:,0] >= 0)
-        #print newweight.dtype,newweight.min(),newweight.max()
         newweight = adj_mask_or((newweight==False),adjustment=5)
         newweight = (newweight == False)
-        #print newweight.dtype,newweight.min(),newweight.max()
         unwarped_newimage[newweight == False] = 0
         unwarped_newimage = ski_exp.rescale_intensity(ski_util.img_as_float(unwarped_newimage))
         imagearr[:,:,:,i] = unwarped_newimage
@@ -105,27 +104,23 @@ if __name__ == "__main__":
         badx = X[newweight == False]
         bady = Y[newweight == False]
         imagearr[bady,badx,:,i] = np.nan
-        #med_image = np.zeros(imagearr[:,:,:,-1].shape)
-        #med_image[:,:,0] = bn.nanmedian(imagearr[:,:,0,:i+1],axis=2)
-        #med_image[:,:,1] = bn.nanmedian(imagearr[:,:,1,:i+1],axis=2)
-        #med_image[:,:,2] = bn.nanmedian(imagearr[:,:,2,:i+1],axis=2)
         avg_image = jointimage*weightimage.reshape(weightimage.shape+(1,))+unwarped_newimage*newweight.reshape(newweight.shape+(1,))
         weightimage += newweight
         avg_image /= weightimage.reshape(weightimage.shape+(1,))
         
         #Replace the currframe with the avg_image:
         currframe = frame(cap,-1,image=ski_util.img_as_ubyte(avg_image))
-        #currframe = frame(cap,-1,image=ski_util.img_as_ubyte(med_image))
-    ski_io.imsave('test_avg.jpg',currframe.rawimage)
+    ski_io.imsave(outprefix+'_avg.jpg',currframe.rawimage)
     medianed_image = np.zeros(imagearr[:,:,:,0].shape)
     medianed_image[:,:,0] = bn.nanmedian(imagearr[:,:,0,:],axis=2)
     medianed_image[:,:,1] = bn.nanmedian(imagearr[:,:,1,:],axis=2)
     medianed_image[:,:,2] = bn.nanmedian(imagearr[:,:,2,:],axis=2)
-    ski_io.imsave('test_median.jpg',medianed_image)
+    ski_io.imsave(outprefix+'_median.jpg',medianed_image)
 
+    #Output the rectified frames:
     for i in range(len(chronological_order)):
         curridx = chronological_order[i]
         print frameorder[curridx],
         currimage = insert_image(medianed_image,imagearr[:,:,:,curridx],adjustmask=1,overlap=20)
-        ski_io.imsave('test_out_{0:d}.jpg'.format(frameorder[curridx]),currimage)
+        ski_io.imsave(outprefix+'_rectified_{0:d}.jpg'.format(frameorder[curridx]),currimage)
 
